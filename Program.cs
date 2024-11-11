dd13082b6e18443e2560d88abaeda4822e3175f5
asikharulidze <ana.sixarulidze@gmail.com>
2024-11-11 17:26:40 +0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace File;
+
+using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+
+        string filePath = "lashas.txt";
+        
+
+        string content = "Love you";
+
+        try
+        {
+
+            File.WriteAllText(filePath, content);
+
+            Console.WriteLine("Lasha is happy.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Lasha is angry: " + ex.Message);
+        }
+    }
+}
